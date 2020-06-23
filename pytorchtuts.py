b7d877aed47ed3ba014d254b7590c44ad1d3906e
Joe Pearson <joe.c.pearson95@gmail.com>
2020-06-23 17:16:40 +0100

    Adding on the previous file

--- pytorchtuts.py
+++ pytorchtuts.py
@@ -2,7 +2,7 @@ import torch
 import torchvision
 from torchvision import transforms, datasets
 
-train = datasets.MNIST("", train=True, download=True, 
+train = datasets.MNIST("", train=True, download=True,
                     transform = transforms.Compose([transforms.ToTensor()]))
 
 test = datasets.MNIST("", train=False, download=True,
@@ -12,27 +12,29 @@ test = datasets.MNIST("", train=False, download=True,
 trainset = torch.utils.data.DataLoader(train, batch_size=10, shuffle=True)
 testset = torch.utils.data.DataLoader(test, batch_size=10, shuffle=True)
 
-#print data
-for data in trainset:
-    print(data)
-    break
+import torch.nn as nn
+import torch.nn.functional as F
 
-#print images       tensors
-x, y = data[0][0], data[1][0]
-#print(y)
-import matplotlib.pyplot as plt
-plt.imshow(data[0][0].view(28,28))
-plt.show()
+# neural network class, inherriting the nn module.
+# init is comprised of a self initialising part from nn.Module and creating a
+# fully conected 1 (N) linear nn. Taking the images (input = 28*28 image) and hidden layers (3 layer 64 node), giving out 10 classes/neurons (numbers)
+class Net(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.fc1 = nn.Linear(28*28, 64)
+        self.fc2 = nn.Linear(64, 64)
+        self.fc3 = nn.Linear(64, 64)
+        self.fc4 = nn.Linear(64, 10)
 
-# count the amounts and present as percentage
-total = 0
-counter_dict =  {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0}
+    # feed forward creation using relu (Rectified Linear Unit) instead of any other activation function right now
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x))
+        x = self.fc4(x)
+        return F.log_softmax(x, dim=1)  # softmax using 'x' and specified dimension - similar to axis
 
-for data in trainset:
-    Xs, Ys = data
-    for y in Ys:
-        counter_dict[int(y)] += 1
-        total += 1
-
-for i in counter_dict:
-    print(f"{i}: {counter_dict[i]/total*100}")
+net = Net()
+X = torch.rand((28,28))
+X = X.view(1, 28*28)
+print(net(X))
